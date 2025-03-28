1738561180283904da31e59a8a951b3dd54310e5
Harshil Motta <harshilmotta26@gmail.com>
2025-03-28 20:21:47 +0530

    changes

--- src/components/main/Certification/Certification.tsx
+++ src/components/main/Certification/Certification.tsx
@@ -6,7 +6,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "./styles.css";
 
-export const Cerifiation = () => {
+export const Cerification = () => {
     return (
         <>
             {/* Section Title */}
